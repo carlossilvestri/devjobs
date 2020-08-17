c902501e0e92dc0bd3932950dba709b0da0b00f7
carlossilvestri <54336195+carlossilvestri@users.noreply.github.com>
2020-08-16 21:35:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -80,10 +80,11 @@ app.use((error, req, res, next) => {
     res.render("error");
 });
 //Dejar que Heroku asigne el puerto a la aplicacion.
-const host = '0.0.0.0';
-const port = process.env.PORT;
+//Servidor y puerto
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
 
 app.listen(port, host, () => {
-    console.log('El servidor esta funcionando.');
+    console.log('El servidor esta funcionando' + ' en el puerto ' + port + ' y en el host ' + host);
 
 });
